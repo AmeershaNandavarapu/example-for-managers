79262fc796930bf24c488b8c3b03f521346c0b45
AmeershaNandavarapu <n.ameersha786@gmail.com>
2022-08-29 03:46:58 +0000

    new styles has been added to login forma nd installed redux-persist and the error of showing state in redux dev tools has been gone automatically

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -9,8 +9,8 @@ function LoginForm(){
         <div>
             <h1>Login form</h1>
             <div className="form">
-                <input type="text" placeholder="Username" value={username} onChange={(event)=>dispatch(userNameChange(event.target.value))}/>
-                <input type="password" placeholder="Password" value={password} onChange={(event)=>dispatch(passwordChange(event.target.value))}/>
+                <input type="text" placeholder="Username" value={username} onChange={(event)=>dispatch(userNameChange(event.target.value))} className="inputc"/>
+                <input type="password" placeholder="Password" value={password} onChange={(event)=>dispatch(passwordChange(event.target.value))} className="inputc"/>
                 <button onClick={()=>dispatch(loggedIn())}>Login</button>
             </div>
         </div>
